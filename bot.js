3149cd47bd81c0046812f08831841c6ae6781b94
Michael Zhang <failed.down@gmail.com>
2015-12-30 21:43:32 +0000

    test

--- bot.js
+++ bot.js
@@ -195,19 +195,23 @@ bot.on("message", function(user, userID, channelID, message, rawEvent) {
 				break;
 			case "ytplaylist":
 				var id = message.substring(11).trim();
-				request("https://www.googleapis.com/youtube/v3/playlistItems?key=" + process.env.YOUTUBE_APIKEY + "&part=contentDetails&playlistId=" + id + "&maxResults=25", function(error, response, body) {
-					var result = JSON.parse(body)["items"];
-					(function next(i) {
-						if (i == result.length) {
-							show_queue();
-							return;
-						} else {
-							add_url_to_queue("https://youtu.be/" + result[i]["contentDetails"]["videoI"], function() {
-								next(i + 1);
-							});
-						}
-					})(0);
-				});
+				if (id.length < 1) {
+					return bot.sendMessage({ to: channelID, message: "GIVE ME A GODDAMN PLAYLIST ID" });
+				} else {
+					request("https://www.googleapis.com/youtube/v3/playlistItems?key=" + process.env.YOUTUBE_APIKEY + "&part=contentDetails&playlistId=" + id + "&maxResults=25", function(error, response, body) {
+						var result = JSON.parse(body)["items"];
+						(function next(i) {
+							if (i == result.length) {
+								show_queue();
+								return;
+							} else {
+								add_url_to_queue("https://youtu.be/" + result[i]["contentDetails"]["videoI"], function() {
+									next(i + 1);
+								});
+							}
+						})(0);
+					});
+				}
 				break;
 			case "ytsearch":
 				var query = message.substring(9).trim();
